4f3de455eb712ecc3e966e55226dfde4524a6c9d
hafsa-fatima <hafsa.fatima995@gmail.com>
2019-02-16 15:28:46 -0600

    'k'

---
+++ homefull/src/components/Signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+export default class MainRouter extends Component {
+    render() {
+        return (
+            <Router>
+
+            </Router>
+        )
+    }
+}
