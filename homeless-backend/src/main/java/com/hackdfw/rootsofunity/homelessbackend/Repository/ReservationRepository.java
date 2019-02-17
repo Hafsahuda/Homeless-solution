087a1007eeb1a50ce0ed71d5ef659e3f917fa34f
Swathygsb <swathygsb@gmail.com>
2019-02-16 19:41:51 -0600

    S 3

--- homeless-backend/src/main/java/com/hackdfw/rootsofunity/homelessbackend/Repository/ReservationRepository.java
+++ homeless-backend/src/main/java/com/hackdfw/rootsofunity/homelessbackend/Repository/ReservationRepository.java
@@ -1,12 +1,10 @@
 package com.hackdfw.rootsofunity.homelessbackend.Repository;
-package org.apache.maven.maven;
-
-import com.hackdfw.rootsofunity.homelessbackend.Domain.Availability;
+import java.util.Date;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
-import java.util.Set;
+import com.hackdfw.rootsofunity.homelessbackend.Domain.Reservation;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 	
 	Set<Reservation> getReservationsByIsBookedfromAndIsBookedafter(long reserve_id, long room_id, long char_id,Date booked_from, Date booked_to);
