d18d4e8b2b1d2edcf6528b338aed1d0cdf9beb73
Swathygsb <swathygsb@gmail.com>
2019-02-17 09:14:16 -0600

    Update Charity.js

--- homefull/src/screens/Charity.js
+++ homefull/src/screens/Charity.js
@@ -6,6 +6,10 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import HouseCard from '../modules/Card';
 import Grid from '@material-ui/core/Grid';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import classNames from 'classnames';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
 class Charity extends Component {
 
@@ -13,8 +17,15 @@ class Charity extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            jsn:[]
+            jsn: [],
+            searchZip: '75252',
+            from:'17-02-2019',
+            to:'18-02-2019'
         };
+        this.saveZip = this.saveZip.bind(this);
+        this.saveFromDate = this.saveFromDate.bind(this);
+        this.saveToDate = this.saveToDate.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
 
 
@@ -22,7 +33,7 @@ class Charity extends Component {
 
     componentDidMount() {
 
-       // let jsn = [];
+        // let jsn = [];
         fetch('http://ec2-3-82-226-163.compute-1.amazonaws.com:8080/room/search?fromDate=17-02-2019&toDate=18-02-2019&zipcode=75252',
             {
                 crossDomain: true
@@ -30,30 +41,95 @@ class Charity extends Component {
             .then(response => response.json())
             .then((data) =>
                 this.setState({
-                    jsn: data
+                    jsn: data,
+                    szip: '75252'
                 }));
     }
+
+
+    handleSearch(e) {
+        e.preventDefault();
+        var self = this;
+        var x = this.state.szip, from = this.state.from, to = this.state.to;
+        var url = 'http://ec2-3-82-226-163.compute-1.amazonaws.com:8080/room/search?fromDate='+from+'&toDate='+to+'&zipcode=' + x;
+        fetch(url,
+            {
+                crossDomain: true
+            })
+            .then(response => response.json())
+            .then((data) =>
+                self.setState({
+                    jsn: data,
+                    szip: x
+                }));
+
+    }
+
+    saveZip(e) {
+        e.preventDefault();
+        // alert(e.target.value);
+        this.setState({
+            szip: e.target.value
+        });
+    }
+
+    saveFromDate(e) {
+        e.preventDefault();
+        // alert(e.target.value);
+        this.setState({
+            from: e.target.value
+        });
+    }
+
+    saveToDate(e) {
+        e.preventDefault();
+        // alert(e.target.value);
+        this.setState({
+            to: e.target.value
+        });
+    }
+
 
     render() {
         //const { classes, theme } = this.props;
         console.log(this)
         return (
 
-            <div>
+            <div style={{ color: 'white' }}>
+                <h1 >
+                    Search Rooms
+                </h1>
+
+                <div >
+
+                    <SearchIcon onClick={this.handleSearch} />
+
+                    <InputBase style={{ color: 'white' }}
+                        placeholder="Search…" onChange={this.saveZip} />
+<br/>
+                    <InputBase style={{ color: 'white' }}
+                        placeholder="From Date (DD-MM-YYYY)" onChange={this.saveFromDate} />
+<br/>
+                    <InputBase style={{ color: 'white' }}
+                        placeholder="To Date (DD-MM-YYYY)" onChange={this.saveToDate} />
+                </div>
+
+                <br />
+
                 <Typography variant="h1" style={{ color: 'white', textAlign: 'center' }} paragraph>
-                   {this.state.jsn.map((obj)=>(
-			     <Grid >
-                     {obj.availability.map((avaObj)=>{
-                          var fd = avaObj.availableFrom;
-                            return <HouseCard startDate={fd} endDate={avaObj.availableTo}
-                                address={obj.room.address}
-                                city={obj.room.city} state={obj.room.state} zipcode={obj.room.zip}
-                                roomId={obj.room.roomId} />
-                                })}
+                    {this.state.jsn.map((obj) => (
+                        <Grid >
+                            {obj.availability.map((avaObj) => {
+                                var fd = avaObj.availableFrom;
+                                return <HouseCard startDate={fd} endDate={avaObj.availableTo}
+                                    address={obj.room.address}
+                                     state={obj.room.state } zipcode={obj.room.zipcode}
+                                    roomId={obj.room.roomId} availId={avaObj.availabilityId}/>
+                            })}
                         </Grid>
-                   ))}
+                    ))}
                 </Typography>
-                
+
 
 
 
@@ -73,5 +149,6 @@ Charity.propTypes = {
     classes: PropTypes.object.isRequired,
     theme: PropTypes.object.isRequired,
 };
+
 
 export default withStyles(styles)(Charity);
