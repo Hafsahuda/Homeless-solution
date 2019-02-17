80af9cc26cae7e80f83a9d4bd7147e72484bb9f0
Venkatakrishnan Srinivasaraj <venkatakrishnan111@gmail.com>
2019-02-17 04:07:14 -0600

    Updated with cors header

--- homeless-backend/src/main/java/com/hackdfw/rootsofunity/homelessbackend/Controller/AvailabilitySearchController.java
+++ homeless-backend/src/main/java/com/hackdfw/rootsofunity/homelessbackend/Controller/AvailabilitySearchController.java
@@ -1,14 +1,17 @@
 package com.hackdfw.rootsofunity.homelessbackend.Controller;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
+import com.sun.glass.ui.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.hackdfw.rootsofunity.homelessbackend.Domain.RoomAvailability;
 import com.hackdfw.rootsofunity.homelessbackend.Service.RoomAvailabilitySearchService;
@@ -21,9 +24,11 @@ public class AvailabilitySearchController {
     @Autowired
     RoomAvailabilitySearchService roomAvailabilitySearchService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "")
-    public @ResponseBody Set<RoomAvailability> getAvailableRooms(@RequestParam @DateTimeFormat(pattern="dd-MM-yyyy")Date fromDate, @RequestParam @DateTimeFormat(pattern="dd-MM-yyyy") Date toDate, @RequestParam String zipcode){
-        return roomAvailabilitySearchService.search(fromDate, toDate, zipcode);
+    public Set<RoomAvailability> getAvailableRooms(@RequestParam @DateTimeFormat(pattern="dd-MM-yyyy")Date fromDate, @RequestParam @DateTimeFormat(pattern="dd-MM-yyyy") Date toDate, @RequestParam String zipcode){
+        Set<RoomAvailability> results = roomAvailabilitySearchService.search(fromDate, toDate, zipcode);
+        return results;
     }
 
 }
