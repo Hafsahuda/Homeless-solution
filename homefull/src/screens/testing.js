a9b8ff64ec935123eeae462beaf0cbc002003b17
Shaquille Wyan Que <shaquillewyanque@Shaquilles-MacBook-Pro.local>
2019-02-16 20:57:38 -0600

    added find charity page

---
+++ homefull/src/screens/testing.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import HouseCard from '../modules/Card';
+
+export default class Test extends Component {
+	render() {
+		return (
+			<HouseCard></HouseCard>
+		)
+	}
+}
