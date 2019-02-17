c2cf4ea8bd00bb89d802a4cf3757afe628620422
Venkatakrishnan Srinivasaraj <venkatakrishnan111@gmail.com>
2019-02-17 06:23:24 -0600

    Added Date format

--- homeless-backend/src/main/java/com/hackdfw/rootsofunity/homelessbackend/Domain/Reservation.java
+++ homeless-backend/src/main/java/com/hackdfw/rootsofunity/homelessbackend/Domain/Reservation.java
@@ -1,9 +1,11 @@
 package com.hackdfw.rootsofunity.homelessbackend.Domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -19,8 +21,12 @@ public class Reservation {
     @GeneratedValue
     private long reservationId;
 
+    @DateTimeFormat(pattern="dd-MM-yyyy")
+    @JsonFormat(pattern="dd-MM-yyyy")
     private Date bookFrom;
 
+    @DateTimeFormat(pattern="dd-MM-yyyy")
+    @JsonFormat(pattern="dd-MM-yyyy")
     private Date bookTo;
 
     @OneToOne
